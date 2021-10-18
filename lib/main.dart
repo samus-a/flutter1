5f096d93e448f60625d8f1512330d06cb745cbc0
samus-a <samusarin@gmail.com>
2021-10-18 08:57:19 -0400

    Favorites button and view navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -14,6 +16,13 @@ class MyApp extends StatelessWidget {
     // return MaterialApp();
     return MaterialApp(
       title: 'Welcome to Flutter',
+      // theme: ThemeData(
+        darkTheme: ThemeData(
+          appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.cyanAccent
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
@@ -38,6 +47,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
@@ -46,9 +56,48 @@ class _RandomWordsState extends State<RandomWords> {
     // return Text(wordPair.asPascalCase);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Name Generator')
-      ),
+        title: const Text('Name Generator'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions'),
+          ],
+        ),
       body: _buildSuggestions()
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute < void > (
+        builder: (context) {
+          //Iterable<T> map<T>(T toElement(E e)) => MappedIterable<E, T>(this, toElement);
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+                tileColor: Colors.black12,
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+            // backgroundColor: Colors.white12
+          );
+        },
+      ),
     );
   }
   Widget _buildSuggestions() {
@@ -66,11 +115,27 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      tileColor: Colors.black12,
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save'
+      ),
+      onTap:() {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
